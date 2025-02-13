8bb29fb15d9ea4ad3fb0ed6c59aeca2ac0e8cefa
Joaol324j <joaol324j@gmail.com>
2025-02-13 16:19:59 -0300

    Adição de validação no username

--- app/routes/users.py
+++ app/routes/users.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
 from app.schemas import UserCreate, UserOut
 from app.crud import get_user, create_user
-from app.validators import valid_email, strong_password
+from app.validators import valid_email, strong_password, valid_username
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -14,6 +14,9 @@ async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
     
     if not strong_password(user.password):
         raise HTTPException(status_code=400, detail="A senha deve ter pelo menos 8 caracteres, uma letra maiúscula, um número e um caractere especial")
+    
+    if not valid_username(user.username):
+        raise HTTPException(status_code=400, detail="O nome de usuário deve ter entre 3 e 20 caracteres e pode conter apenas letras, números e underscores")
 
     existing_user = await get_user(db, user.username)
     if existing_user:
--- app/validators.py
+++ app/validators.py
@@ -7,3 +7,7 @@ def valid_email(email: str) -> bool:
 def strong_password(password: str) -> bool:
     password_regex = r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
     return re.match(password_regex, password) is not None
+
+def valid_username(username: str) -> bool:
+    username_regex = r'^[a-zA-Z0-9_]{3,20}$'
+    return re.match(username_regex, username) is not None
